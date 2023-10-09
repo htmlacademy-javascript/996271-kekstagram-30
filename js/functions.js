7055412a2cdb2d9ca0dbf24c99fea0c9b54f503a
kiyotaka <sintyay.bogdan53@gmail.com>
2023-10-10 00:45:44 +0400

    Add comments

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,5 @@
+// Функция для проверки длины строки
+
 function lengthCheck(stringName, maxlength) {
   if (stringName.length <= maxlength) {
     return true;
@@ -7,15 +9,17 @@ function lengthCheck(stringName, maxlength) {
 
 lengthCheck('123', 3);
 
+// Функция на проверку полиндрома
+
 function isPalindrome(string) {
-  string = string.toLowerCase().replaceAll(/ /g, '');
-  const stringArray = [...string];
-  const newArray = [];
-  stringArray.forEach((index) => {
+  string = string.toLowerCase().replaceAll(/ /g, ''); // Приводим строку к нижнему регистру и удаляем все пробелы
+  const stringArray = [...string]; // Записываем строку в массив
+  const newArray = []; // Создаем пустой массив
+  stringArray.forEach((index) => { // С помощью цикла и метода unshift перезаписываем строку в пустой массив
     newArray.unshift(index);
   });
-  const reversedString = newArray.join('');
-  if (string === reversedString) {
+  const reversedString = newArray.join(''); // Создаем новую переменную и записываем результат из цикла
+  if (string === reversedString) { // Сравниваем и возвращаем результат
     //console.log('Это палиндром');
     return string === reversedString;
   }
