36032aaf71206c83e3b52cf536e9767ff9c1e107
kiyotaka <sintyay.bogdan53@gmail.com>
2023-11-05 22:06:26 +0400

    picture

---
+++ js/picture.js
@@ -0,0 +1,19 @@
+import { getPicture } from './data.js';
+
+const picContainer = document.querySelector('.pictures');
+const picTemplate = document.querySelector('picture').content.querySelector('.picture');
+
+const createThumbnails = getPicture();
+
+const pictureFragment = document.createDocumentFragment();
+
+createThumbnails.forEach(({url, description, likes, comments}) => {
+  const picElement = picTemplate.cloneNode(true);
+  picElement.querySelector('.picture__img').src = url;
+  picElement.querySelector('.picture__info').alt = description;
+  picElement.querySelector('.picture__likes').textContent = likes;
+  picElement.querySelector('.picture__comments').textContent = comments.length;
+  pictureFragment.append(picElement);
+});
+
+picContainer.append(pictureFragment);
