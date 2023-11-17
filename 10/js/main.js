ff060b8eecb552293b798afb83659b8c06413422
Keks <keks@htmlacademy.ru>
2023-11-18 02:18:24 +0300

    :heavy_check_mark: Сборка #10

---
+++ 10/js/main.js
@@ -0,0 +1,6 @@
+import { getPicture } from './data.js';
+import './utils.js';
+import { renderGallery } from './gallery.js';
+import './form.js';
+
+renderGallery(getPicture());
---
+++ 10/js/picture.js
@@ -0,0 +1,98 @@
+const COMMENTS_COUNT_SHOW = 5;
+
+const bigPictureElement = document.querySelector('.big-picture');
+const bodyElement = document.querySelector('body');
+const closePictureButtonElenet = bigPictureElement.querySelector('.big-picture__cancel');
+
+// переменные коментов
+
+const commentElement = document.querySelector('#comment')
+  .content.querySelector('.social__comment');
+const commentsListElement = bigPictureElement.querySelector('.social__comments');
+const commentsCountElement = bigPictureElement.querySelector('.social__comment-shown-count');
+const totalCommentCountElement = bigPictureElement.querySelector('.social__comment-total-count');
+const commentsLoader = bigPictureElement.querySelector('.comments-loader');
+
+let commentsCountShown = 0;
+let comments = [];
+
+const hidePicture = () => {
+  commentsCountShown = 0;
+  bigPictureElement.classList.add('hidden');
+  bodyElement.classList.remove('model-open');
+  document.removeEventListener('keydown', onDocumentKeydown);
+};
+
+const onClosePictureButtonClick = () => {
+  hidePicture();
+};
+
+function onDocumentKeydown(evt) {
+  if (evt.key === 'Escape') {
+    evt.preventDefault();
+    hidePicture();
+  }
+}
+
+closePictureButtonElenet.addEventListener('click', onClosePictureButtonClick);
+
+const renderPicture = ({ url, description, likes }) => {
+  bigPictureElement.querySelector('.big-picture__img img').src = url;
+  bigPictureElement.querySelector('.big-picture__img img').alt = description;
+  bigPictureElement.querySelector('.likes-count').textContent = likes;
+  bigPictureElement.querySelector('.social__caption').textContent = description;
+};
+
+const createComment = ({ avatar, message, name }) => {
+  const newComment = commentElement.cloneNode(true);
+
+  newComment.querySelector('.social__picture').src = avatar;
+  newComment.querySelector('.social__picture').alt = name;
+  newComment.querySelector('.social__text').textContent = message;
+
+  return newComment;
+};
+
+const renderComments = () => {
+  commentsCountShown += COMMENTS_COUNT_SHOW;
+
+  if (commentsCountShown >= comments.length) {
+    commentsLoader.classList.add('hidden');
+    commentsCountShown = comments.length;
+  } else {
+    commentsLoader.classList.remove('hidden');
+  }
+
+  const fragment = document.createDocumentFragment();
+  for (let i = 0; i < comments.length; i++) {
+    const comment = createComment(comments[i]);
+    fragment.append(comment);
+  }
+  // Проблема с циклом, не знаю как это решить
+  commentsListElement.innerHTML = '';
+  commentsListElement.append(fragment);
+
+  commentsCountElement.textContent = commentsCountShown;
+  totalCommentCountElement.textContent = comments.length;
+};
+
+const showPicture = (pictureData) => {
+  bigPictureElement.classList.remove('hidden');
+  bodyElement.classList.add('model-open');
+  document.addEventListener('keydown', onDocumentKeydown);
+
+  comments = pictureData.comments;
+  if (comments.length > 0) {
+    renderComments();
+  }
+
+  renderPicture(pictureData);
+};
+
+const onCommentsLoaderClick = () => {
+  renderComments();
+};
+
+commentsLoader.addEventListener('click', onCommentsLoaderClick);
+
+export { showPicture };
